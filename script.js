51c6ebeb268c2236a4f950693968c9f5a0ba608a
Skinny_Vin <118040316+SkinnyVin1@users.noreply.github.com>
2023-02-15 14:55:04 +0800

    minor changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // active navbar
 let nav = document.querySelector(".wraper");
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 100){
+    if(document.documentElement.scrollTop > 50){
         nav.classList.add("scrollOn");
     }else{
         nav.classList.remove("scrollOn");
