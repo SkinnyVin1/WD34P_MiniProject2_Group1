1763d4ac5e8b7afb69aa2a9c3a0b44dbd6f3256b
Skinny_Vin <simonvicente38@gmail.com>
2023-02-21 23:36:27 +0800

    added a testimonials section

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ window.onscroll = function () {
     }
 }
 
-// 
+// TESTIMONIALS
+const testimonials = document.querySelector('.testimonials');
+const testimonialItems = testimonials.querySelectorAll('.testimonial');
+let index = 0;
+
+function showNextTestimonial() {
+  // Hide the current testimonial
+  testimonialItems[index].classList.remove('active');
+  // Increment the index
+  index++;
+  // If the index is greater than the number of testimonials, start over at 0
+  if (index >= testimonialItems.length) {
+    index = 0;
+  }
+  // Show the next testimonial
+  testimonialItems[index].classList.add('active');
+}
+
+// Show the first testimonial
+testimonialItems[index].classList.add('active');
+// Set an interval to show the next testimonial every 5 seconds
+setInterval(showNextTestimonial, 5000);
+
