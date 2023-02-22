cdf66272aee425d718ce4af514a04b3a2182e540
emman0779 <emmangalaroza@gmail.com>
2023-02-22 21:49:51 +0800

    changes

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // active navbar
 let nav = document.querySelector(".wraper");
+let maps = document.querySelector(".mapLocation");
+let locationKingdom = document.querySelector(".locationKingdom");
+let locationBurger = document.querySelector(".locationBurger");
+
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 50){
         nav.classList.add("scrollOn");
     }else{
         nav.classList.remove("scrollOn");
+    };
+
+    if(document.documentElement.scrollTop > 200){
+        maps.classList.add("mapActive");
+        setTimeout(function(){
+            maps.classList.add("mapBorder");
+            locationBurger.style.display = "grid"
+        },5000);
+        setTimeout(function(){
+            locationKingdom.style.display = "grid"
+        },6000)
     }
 }
+
 
 // 
